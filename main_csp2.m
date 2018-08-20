67f7d4e102dfe2b2cfb8685ff6b7a64abeccdc6e
kingcheng2000 <87102076@qq.com>
2018-08-20 02:09:56 +0100

    Z caculation and X1 ,X2 X7 X8

--- main_csp2.m
+++ main_csp2.m
@@ -7,7 +7,7 @@ warning off
 addpath(genpath('functions'))
 data = load('20180808215944_MI_chengdan_MI01.easy');
 
-epoch_range = [-0.5 5.5];
+epoch_range = [0.5 4.5];
 time_ranges = [0 5];
 lambda = 0.1;
 cross_wait = 5;
@@ -17,7 +17,7 @@ EEG.srate = 500;
 Fs = EEG.srate ;
 EEG.data = data;
 EEG.epoch = [];
-EEG.filename = 'Motor Imagination' ;
+EEG.filename = 'Motor Imagery' ;
 
 % find  markers
 flag_M = find(data(:,9) ~= 0 );
@@ -37,8 +37,8 @@ end
 %% Epoch extraction
 % window for motor imagination 5s and relax 4.5s. For each epoch will eill
 % add -0.5 seconds before action start and 0.5 seconds after action end
-rang_M = [-0.5 5.5] ;
-rang_R = [-0.5 5.0] ;
+rang_M = [0.5 4.5] ;
+rang_R = [0.5 4.0] ;
 
 epochM = [];
 for i = 1: size(flag_M, 1)
@@ -49,7 +49,7 @@ end
 epochR = [];
 for i = 1: size(flag_R, 1)
     wnd_r = rang_R * Fs +  flag_R(i); 
-    epochR = [epochM;EEG.data(wnd_r(1):wnd_r(2),1:8)];
+    epochR = [epochR;EEG.data(wnd_r(1):wnd_r(2),1:8)];
 end
 EEG.epoch = {epochM , epochR};
 EEG.epoch_data = [epochM;epochR];
@@ -63,12 +63,13 @@ EEG.EpoFltData{k} = filtfilthd(b,a,EEG.epoch{k});
 end
 
 %% feature extraction  CSP
-C1_Data = EEG.EpoFltData{1}(1:2000,1:8)' ;
-C2_Data = EEG.EpoFltData{2}(1:1750,1:8)' ;
+C1_Data = EEG.EpoFltData{1}(250:2250,1:8)' ;
+C2_Data = EEG.EpoFltData{2}(250:2000,1:8)' ;
 
 [W] = f_CSP(C1_Data,C2_Data);
 % spatial filtered singel trail signal Z
-singel_trail = [C1_Data,C2_Data];
+singel_trail = EEG.data(flag_M:(flag_R + 4.5*Fs),1:8)';
+singel_trail = [EEG.EpoFltData{1}(1:3000,1:8)',EEG.EpoFltData{1}(1:2750,1:8)'];
 Z = W * singel_trail;
 % plot  CSP filtered EEG data
  figure 
@@ -83,7 +84,6 @@ Z = W * singel_trail;
  
  subplot(4,1,4)
  plot(Z(8,:))
-
 
 % log-variance feature extraction
 deno = var(Z(1,:)) + var(Z(2,:))+ var(Z(7,:))+ var(Z(8,:)) ;
